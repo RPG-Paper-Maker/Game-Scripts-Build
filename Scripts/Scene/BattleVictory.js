09fc8be2f5e7849436c1555ae5080e0c2c38566f
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-05-01 14:35:46 +0200

    Fix dead battlers change pose on end of battle

--- Scripts/Scene/BattleVictory.js
+++ Scripts/Scene/BattleVictory.js
@@ -36,7 +36,9 @@ class BattleVictory {
         for (battler of this.battle.battlers[CHARACTER_KIND.HERO]) {
             s = battler.player.status[0];
             battler.player.removeEndBattleStatus();
-            battler.updateStatusStep();
+            if (!battler.player.isDead()) {
+                battler.updateStatusStep();
+            }
             battler.updateAnimationStatus(s);
         }
         // Clear status animations for all battlers (visual only)
